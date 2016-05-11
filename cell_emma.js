f2e8fa245e081bbca7be5f154ccb7610f2306275
likhona <likhonapr@projectcodex.co>
2016-05-11 13:58:00 +0200

    latest

--- cell_emma.js
+++ cell_emma.js
@@ -1,27 +1,69 @@
-exports.objarr = function(){
+exports.manipulateArr = function(){
 
-  var users = "Walter sent: “It was 67MB. Is that everything? I’m busy, you know.”|Jesse sent: “Damn, it was like 300MB. Dat Snaptalk is a data hog, yo.”|Saul sent: “I think 283MB. That’s what the guy at the store said. I think it means MegaBites.”|Gus sent: “Around 150MB. I have the receipts if you need more precise figures.”";
+var users = "Walter sent: “It was 67MB. Is that everything? I’m busy, you know.”Jesse sent: “Damn, it was like 300MB. Dat Snaptalk is a data hog, yo.”Saul sent: “I think 283MB. That’s what the guy at the store said. I think it means MegaBites.”Gus sent: “Around 150MB. I have the receipts if you need more precise figures.”";
 
+var user = users.replace(/MB/g, " MB").replace(/”/g, "").split(".");
+var user1 = [];var user2 = [];
 
-  var user = users.split(".");
-  var userData = [];
-  var userData2 = [];
-// var userData3 = [];
+user.forEach(function(str) {
+  user1.push(str.split(' '));
+});
+user1.forEach(function(arr) {
+  if (arr[arr.length - 1] === "MB") {
+    user2.push(arr);
+  }
+});
+// console.log(user1);
+//  console.log(user2);
 
-    userData.push(user[0]);userData.push(user[2]);
-    userData.push(user[4]);userData.push(user[7]);
+var userData = [];
 
-userData.forEach(function(arr){
-userData2.push(arr.split(","));
+user2.forEach(function(arr) {
 
+  userObj = {
+    name: arr[0],
+    data: Number(arr[arr.length - 2])
+  };
+
+  userData.push(userObj);
 
 });
-console.log(userData2);
+//console.log(userData);
+return userData;
+}
+//////////////////////////////////////////////////////////////////////////////
+exports.maximum = function(ObjectData){
 
-//var replasUser = userData2.replace(/"|"/g, "");
+  var max = -Infinity;
+   var maxObj = {};
+   ObjectData.forEach(function(obj) {
+     if (obj.data > max) {
+        max = obj.data;
+        maxObj = obj;
+}
+
+})
+
+//console.log(maxObj)
+return maxObj;
+}
+/////////////////////////////////////////////////////////////////////////////
+exports.minimum = function(ObjectData){
+
+var min = Infinity;
+var minObj = {};
+ObjectData.forEach(function(obj){
+  if(obj.data < min){
+
+    min = obj.data;
+    minObj = obj;
+
+  }
+
+})
+
+//console.log(minObj);
+return minObj;
 
 
-//console.log(replasUser);
-  });
-  console.log(userData2);
 }
--- test/Burners-And-Earners-test.js
+++ test/Burners-And-Earners-test.js
@@ -1,50 +1,61 @@
-var asserts = require("assert");
+var assert = require("assert");
 
 var cell_emma = require("../cell_emma");
 
 
+var ObjectData = [ { name: 'Walter', data: 67 },
+  { name: 'Jesse', data: 300 },
+  { name: 'Saul', data: 283 },
+  { name: 'Gus', data: 150 } ]
+
+
 describe("Take the string inputs above and calculate", function(){
 
-// it("", function(){
-// var result = cell_emma();
-// assert.deep(result,[])
-//
-// })
+ it("should return the Object with usernames and data values", function(){
+var vee =   [ { name: 'Walter', data: 67 },
+     { name: 'Jesse', data: 300 },
+     { name: 'Saul', data: 283 },
+     { name: 'Gus', data: 150 } ]
+
+ var result = cell_emma.manipulateArr(ObjectData);
+ assert.deepEqual(result, vee)
+
+ })
 
 
 
 
 it("should return user who uses most data", function(){
-// var result = cell-emma();
-// asser.deepEqual(reslut, {Jesse:300 MB})
-
-  // it('should give an array', function() {
-  //     var vee = ['1 3', '2 7', '3 10', '5 14.50'];
-  //     var result = bestDeal.Arr(strAvo);
-  //     assert.deepEqual(result, vee);
+  var vee1 = {name: 'Jesse', data: 300};
+var result = cell_emma.maximum(ObjectData);
+assert.deepEqual(result,vee1)
 })
 
 it("should return user who uses the least data", function(){
 
-
-
+var vee2 = { name: 'Walter', data: 67 };
+var result = cell_emma.minimum(ObjectData);
+assert.deepEqual(result,vee2)
 })
 
 
 it("should return user Who uses the most apps", function(){
-
-
+var result = cell_emma();
+assert.equal(result)
 })
 it("should return Who uses Faceblock?", function(){
-
+  var result = cell_emma();
+  assert.equal(result)
 })
 
 it("should return Who doesn’t use Faceblock?", function(){
-
+  var result = cell_emma();
+  assert.equal(result)
 })
 
 it("should return For each person, estimate how much data each app uses. Use the data ratios from our friend at the network.", function(){
-
+  var result = cell_emma();
+  assert.equal(result)
 })
 
 })
